40f8f9cbf87ba8a48367eb9cc87bb76385b86334
Hogarth16352 <hogarth16352@gmail.com>
2023-04-07 15:19:25 -0600

    docs actualizado

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -17,7 +17,7 @@ export const HeroCard = ( { id,
                             characters
 } ) => {
 
-  const heroImageUrl = `/heroes/${ id }.jpg`;
+  const heroImageUrl = `/public/heroes/${ id }.jpg`;
 
   // const charactersByHero = <p> { characters } </p>;
                             
